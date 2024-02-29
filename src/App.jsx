7bfe7d3eb9edaae28909d995e2210be7b3cdb12d
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-02-29 17:54:59 -0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "./components/sidebar";
+
+function App() {
+  return (
+    <main>
+     <Sidebar />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+export default function Sidebar() {
+  return (
+    <aside>
+      <h2> All Projects</h2>
+      <div>
+      <button> + Add project </button>
+      </div>
+      <ul></ul>
+      </aside>);
+}
