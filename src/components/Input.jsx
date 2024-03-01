8fabd39646a555797624d2e2dee10735f1f53455
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-03-01 12:23:32 -0500

    style added

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,12 @@
 export default function Input({label, textarea, ...props}){
+  
+  const classes = "w-full p-1 borfer-b-2 rounded-sm border-stone-100 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+
   return (
-  <p>
-    <label>{label}</label>
-  {textarea ? <textarea {...props}/> : <input {...props}/>}
+  <p className="flex flex-col gap-1 my-4">
+    <label className="text-sm font-bold uppercase text-sky-500">{label}</label>
+  {textarea ? <textarea className={classes}
+  {...props}/> : <input className={classes} {...props}/>}
   </p>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,10 +2,10 @@ import Input from "./Input"
 
 export default function NewProject(){
   return (
-    <div>
-      <menu>
-        <li><button>Cancel</button></li>
-        <li><button>Save</button></li>
+    <div className="w-[35rem]  mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li><button className="text-sky-800 hover:text-sky-950">Cancel</button></li>
+        <li><button className="px-6 py-2 rounded-md bg-sky-800 text-sky-100 hover:bg-sky-950">Save</button></li>
       </menu>
       <div>
         <Input label='Title'/>
